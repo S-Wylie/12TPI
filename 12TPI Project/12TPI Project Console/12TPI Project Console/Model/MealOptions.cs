fffd97233eef5cc8c12764f7d2f52e0b8e368965
s-wylie <ac152244@avcol.school.nz>
2026-06-25 10:52:33 +1200

    Added comments explaing and adding context to the function of the code to the 'MealOptions' and 'PassengerTickets' table.

--- 12TPI Project/12TPI Project Console/12TPI Project Console/Model/MealOptions.cs
+++ 12TPI Project/12TPI Project Console/12TPI Project Console/Model/MealOptions.cs
@@ -4,13 +4,13 @@ using System.Text;
 
 namespace _12TPI_Project_Console.Model
 {
-    public class MealOptions
+    public class MealOptions //Creates a public class for the 'MealOptions' table of the database
     {
         public String MealCode { get; set; }
         public String Name { get; set; }
         public String Conditions { get; set; }
-        public MealOptions() { }
-        public MealOptions(string m, string n, string c)
+        public MealOptions() { } //Creates the constructor for the 'MealOptions' class
+        public MealOptions(string m /* = MealCode */, string n /* = Name */, string c /* = Conditions*/)
         {
             MealCode = m;
             Name = n;
--- 12TPI Project/12TPI Project Console/12TPI Project Console/Model/PassengerTickets.cs
+++ 12TPI Project/12TPI Project Console/12TPI Project Console/Model/PassengerTickets.cs
@@ -4,15 +4,15 @@ using System.Text;
 
 namespace _12TPI_Project_Console.Model
 {
-    public class PassengerTickets
+    public class PassengerTickets //Creates a public class for the 'PassengerTickets' table of the database
     {
         public int TicketID { get; set; }
         public int FlightID { get; set; }
         public int CustomerID { get; set; }
         public String ClassName { get; set; }
         public String MealChoice { get; set; }
-        public PassengerTickets() { }
-        public PassengerTickets(int t, int f, int ci, string cn, string m)
+        public PassengerTickets() { } //Creates the constructor for the 'PassengerTickets' class
+        public PassengerTickets(int t /* = TicketID */, int f /* = Flight ID */, int ci /* = Customer ID */, string cn /* = ClassName */, string m /* = MealChoice */)
         {
             TicketID = t;
             FlightID = f;
