d13c122ad6973a474395f52435998cf5b43e08c7
s-wylie <ac152244@avcol.school.nz>
2026-06-30 14:04:52 +1200

    Organised Files and created Wireframes for the Landing Page, Admin Login, and Manager Login

--- 12TPI Project/12TPI Project Console/12TPI Project Console/Model/Airports.cs
+++ 12TPI Project/12TPI Project Console/12TPI Project Console/Model/Airports.cs
@@ -4,15 +4,15 @@ using System.Text;
 
 namespace _12TPI_Project_Console.Model
 {
-    public class Airports
+    public class Airports //Creates a public class for the 'Airports' table of the database
     {
         public int IATACode { get; set; }
         public String Name { get; set; }
         public String Coordinates { get; set; }
         public String Country { get; set; }
         public String Timezone { get; set; }
-        public Airports() { }
-        public Airports(int i, string n, string cor, string con, string t)
+        public Airports() { } //Creates the constructor for the 'Passengers' class
+        public Airports(int i /* = IATACode */, string n /* = Name */, string cor /* = Coordinates */, string con /* = Country */ , string t /* Timezone */)
         {
             IATACode = i;
             Name = n;
