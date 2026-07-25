a24a959e2dbeec53d02c5bc684bc0e03cdba477a
s-wylie <ac152244@avcol.school.nz>
2026-07-25 19:06:55 +1200

    Added new class for the Logins table and worked on the wireframe

---
+++ 12TPI Project/12TPI Project Console/12TPI Project Console/Model/Logins.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _12TPI_Project_Console.Model
+{
+    public class Logins //Creates a public class for the 'Logins' table of the database 
+    {
+        public String Username { get; set; }
+        public int PINHash { get; set; }
+        public char AccessLevel { get; set; }
+
+        public Logins() { } //Creates the constructor for the 'Logins' class
+        public Logins(string u /* = Username */, int p /* = PINHash */, char l /* = AccessLevel */)
+        {
+            Username = u;
+            PINHash = p;
+            AccessLevel = l;
+        }
+    }
+}
+
+
